ff0bdf472901e2437b57f6ed56a344543856aeac
RISHIKA SHAH <rishikashah835@gmail.com>
2026-06-27 21:35:22 +0530

    Create  ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,40 @@
+/*
+Problem: Reverse Linked List
+Platform: LeetCode
+Difficulty: Easy
+https://leetcode.com/problems/valid-palindrome/
+Approach: Three Pointers
+Time Complexity: O(1)
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next ==null){
+            return head;
+        }
+        ListNode prevNode=head;
+        ListNode currNode=head.next;
+        while(currNode != null){
+            ListNode nextNode=currNode.next;
+            currNode.next=prevNode;
+            prevNode=currNode;
+            currNode=nextNode;
+
+        }
+        head.next=null;
+        head=prevNode;
+        return prevNode;
+    }
+    
+}
