428d2828b82f2a855f5450a5cbd4193b2d76ec20
RISHIKA SHAH <rishikashah835@gmail.com>
2026-07-10 13:50:39 +0530

    Create Daily Temperatures.java

---
+++ Stack/Daily Temperatures.java
@@ -0,0 +1,25 @@
+/*
+
+Title:Daily Temperatures
+Mode:Medium
+Link:https://leetcode.com/problems/daily-temperatures/submissions/2062576674/
+Approach:Stack
+Time Complexity: O(n)
+
+*/
+
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        int n=temp.length;
+        int[] ans =new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && temp[i]>temp[st.peek()] ){
+                int prev=st.pop();
+                ans[prev]=i-prev;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
