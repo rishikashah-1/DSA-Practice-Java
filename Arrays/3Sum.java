a382d81741cf168c7cc3887b856fbf0989c3dfa3
RISHIKA SHAH <rishikashah835@gmail.com>
2026-08-02 19:41:05 +0530

    Add solution for 3Sum problem
    Implement the 3Sum problem solution using sorting and two-pointer technique.

---
+++ Arrays/3Sum.java
@@ -0,0 +1,44 @@
+/*
+Problem: 3Sum
+Platform: LeetCode
+Difficulty: Medium
+https://leetcode.com/problems/3sum/Time Complexity: O(n)
+
+*/
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> list=new ArrayList<>();
+        for(int i=0;i<n-2;i++){
+        if(i>0 && nums[i]==nums[i-1]){
+            continue;
+        }
+        int l=i+1;
+        int r=n-1;
+        while(l<r){
+            int sum=nums[i]+nums[l]+nums[r];
+            if(sum==0){
+               list.add(Arrays.asList(nums[i],nums[l],nums[r]));
+               while(l<r && nums[l]==nums[l+1]){
+                l++;
+               }
+               while(l<r && nums[r]==nums[r-1]){
+                r--;
+               }
+               l++;
+               r--;
+            }
+            else if(sum<0){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+       }
+       return list;
+    }
+    
+}
