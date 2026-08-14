55bedb73cc12e58be28bc7e0facd51012aa042fa
RISHIKA SHAH <rishikashah835@gmail.com>
2026-08-14 21:34:36 +0530

    Unfinished code attempt 1

--- Tree/Inverted Binary Tree.java
+++ Tree/Inverted Binary Tree.java
@@ -1,1 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        binary(root);
+        return root;
+    }
 
+    public TreeNode binary(TreeNode root){
+          if(root==null){
+            return;
+          }
+          TreeNode left=binary(root.left);
+          TreeNode right=binary(root.right);
+          if(left==null && right==null){
+            return;
+          }
+          swapping(left,right);
+    }
+         public TreeNpde swapping(TreeNode left,TreeNode right){ 
+         TreeNode swap=left;
+         left=right;
+         right=swap;
+         return left,right;
+         }
+    
+} 
