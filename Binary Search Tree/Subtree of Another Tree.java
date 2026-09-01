2ca024ccbf3e2f51cf7883d5bf17755d9cac9b09
RISHIKA SHAH <rishikashah835@gmail.com>
2026-09-01 21:04:13 +0530

    Revise isSubtree method for complete subtree validation
    Enhance isSubtree method to perform a comprehensive subtree check.

--- Binary Search Tree/Subtree of Another Tree.java
+++ Binary Search Tree/Subtree of Another Tree.java
@@ -15,11 +15,35 @@
  */
 
 class Solution {
+
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root.val==subRoot.val){
-           if(subRoot.left.val==root.left.val && subRoot.right.val==root.right.val)
-             return true;
-        }
-         return false;
+
+   
+        if (root == null)
+            return false;
+
+        if (sameTree(root, subRoot))
+            return true;
+
+        return isSubtree(root.left, subRoot) ||
+               isSubtree(root.right, subRoot);
+    }
+
+
+    public boolean sameTree(TreeNode root, TreeNode subRoot) {
+
+
+        if (root == null && subRoot == null)
+            return true;
+
+    
+        if (root == null || subRoot == null)
+            return false;
+
+        if (root.val != subRoot.val)
+            return false;
+
+        return sameTree(root.left, subRoot.left) &&
+               sameTree(root.right, subRoot.right);
     }
 }
