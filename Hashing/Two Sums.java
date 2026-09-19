624730cda11044fc7e894519bdecb23f4ce50c5d
RISHIKA SHAH <rishikashah835@gmail.com>
2026-09-19 21:35:16 +0530

    Implement twoSum method in Solution class

--- Hashing/Two Sums.java
+++ Hashing/Two Sums.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> set=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int check=target-nums[i];
+            if(set.containsKey(check)){
+                return new int[]{i,set.get(check)};
+            }
+        
+        set.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
